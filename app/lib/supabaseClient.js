5a5de458168faac50131e7b08ad5bcf2acc5f86d
ahmedfathy112 <ahmedfathy241110@gmail.com>
2025-09-21 23:35:12 +0300

    fix supabase errpr

--- app/lib/supabaseClient.js
+++ app/lib/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://rodgpnbaewagvfedxbqs.supabase.co";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
 
