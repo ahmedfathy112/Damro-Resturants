4d3ef2692cb4ba7c4a70294663bcfc641a293b26
ahmedfathy112 <ahmedfathy241110@gmail.com>
2025-09-23 16:01:29 +0300

    try

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // مهم للصور المحلية
-  trailingSlash: true,
-  images: {
-    unoptimized: true, // هذا السطر أساسي
-    domains: [
-      "damro-resturants.vercel.app",
-      "rodgpnbaewagvfedxbqs.supabase.co",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "rodgpnbaewagvfedxbqs.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
-  },
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://damro-resturants.vercel.app"
-      : "",
+  // experimental: {
+  //   instrumentationHook: false,
+  // },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "rodgpnbaewagvfedxbqs.supabase.co",
+  //       port: "",
+  //       pathname: "/storage/v1/object/public/**",
+  //     },
+
+  //     {
+  //       protocol: "https",
+  //       hostname: "damro-resturants.vercel.app",
+  //       port: "",
+  //       pathname: "/Images/**",
+  //     },
+  //   ],
+  //   unoptimized: true,
+
+  //   domains: ["damro-resturants.vercel.app"],
+  // },
+
+  // trailingSlash: true,
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(png|jpg|jpeg|gif|svg)$/i,
+  //     type: "asset/resource",
+  //   });
+  //   return config;
+  // },
 };
 
 export default nextConfig;
